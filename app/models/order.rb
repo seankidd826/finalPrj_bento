1b627a8f83cb1a3985fff7f7f5a129de3b1d1242
Sean Tsai <kidd826@gmail.com>
2015-05-07 22:49:31 +0800

    1.Update order後會減去訂單的量 2.新增便當編號在管理者介面

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,6 +4,7 @@ class Order < ActiveRecord::Base
   validates_presence_of :order_phone
   validates_numericality_of :menu_count, :only_integer => true
   before_validation :setup_total
+  after_create :consume_stock
 
   def total
     self.menu_count * self.menu.price
@@ -13,6 +14,12 @@ class Order < ActiveRecord::Base
   # 總價直接存在後台
   def setup_total
     self.total_price = self.total
+  end
+
+  def consume_stock
+    stock = self.menu.in_stock_qty
+    stock = stock - self.menu_count
+    self.menu.update(:in_stock_qty => stock )
   end
 
 end
