43c054ecaeff978954ce80d85ceb34254951b46e
John Chang <tschangc@gmail.com>
2015-05-08 12:21:52 +0800

    Setup admin password yml in server and local

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,14 +1,17 @@
 class AdminController < ApplicationController
-
+  admin_config = YAML.load(File.read("#{Rails.root}/config/admin.yml"))[Rails.env]
   if Rails.env.development?
     http_basic_authenticate_with(
-      name: '12345', password: '12345'
+      # name: '12345', password: '12345'
+      name: admin_config["name"],
+      password: admin_config["passwords"]
     )
   end
 
   if Rails.env.production?
     http_basic_authenticate_with(
-      name: '', password: ''
+      name: admin_config["name"],
+      password: admin_config["passwords"]
     )
   end
   layout 'admin'
