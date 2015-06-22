41ddbb98c1a8490b3794ee393942d84bd3264a70
John Chang <tschangc@gmail.com>
2015-06-23 01:40:25 +0800

    Try another method for mail confirmation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,6 +35,18 @@ Rails.application.configure do
   # yet still be able to expire them through the digest params.
   config.assets.digest = true
   config.action_mailer.default_url_options = { :host => '139.162.8.33' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.smtp_settings = {
+      enable_starttls_auto: true,
+      address:              'smtp.gmail.com',
+      port:                 587,
+      domain:               'gmail.com',
+      user_name:            'youremail@gmail.com',
+      password:             'yourpassword',
+      authentication:       :login
+    }
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
